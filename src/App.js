1de4f89a86ccbbe23837d8186f60b5397384c48e
Mike Esposito <mikeesposito6852@gmail.com>
2022-12-19 13:08:18 -0800

    Import Experience component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 class App extends Component {
   render() {
