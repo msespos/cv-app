1fa5823aee295f759c7b2c0992990e4c4e4965a9
Mike Esposito <mikeesposito6852@gmail.com>
2022-12-19 13:02:22 -0800

    Make class component and render GeneralInfo Education and Experience

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React, { Component } from 'react';
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GeneralInfo />
+        <Education />
+        <Experience />
+      </div>
+    );
+  }
 }
 
 export default App;
