b88b33b69eebb5814866c1adc883edb7deb6901a
Mike Esposito <mikeesposito6852@gmail.com>
2023-01-20 17:51:04 -0800

    Refactor to use function rather than class

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import GeneralInfo from './components/GeneralInfo';
 import Education from './components/Education';
 import Experience from './components/Experience';
 
-class App extends Component {
-  render() {
-    return (
-      <div style={{maxWidth: "60ch", margin: "0 auto"}}>
-        <div style={{fontSize: "xxx-large", textAlign: "center"}}>Resume Builder</div>
-        <GeneralInfo />
-        <div style={{fontSize: "xx-large", textAlign: "center"}}>Education</div>
-        <Education />
-        <div style={{fontSize: "xx-large", textAlign: "center"}}>Experience</div>
-        <Experience />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div style={{maxWidth: "60ch", margin: "0 auto"}}>
+      <div style={{fontSize: "xxx-large", textAlign: "center"}}>Resume Builder</div>
+      <GeneralInfo />
+      <div style={{fontSize: "xx-large", textAlign: "center"}}>Education</div>
+      <Education />
+      <div style={{fontSize: "xx-large", textAlign: "center"}}>Experience</div>
+      <Experience />
+    </div>
+  );
 }
 
 export default App;
